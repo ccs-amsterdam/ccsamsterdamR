36652a73f8ccf908e2a7fc4210e600e07582aba0
masurp <p.k.masur@vu.nl>
2023-11-16 14:40:35 +0100

    Add image classification to hf_api.r.

--- R/hf_api.R
+++ R/hf_api.R
@@ -11,7 +11,8 @@
 #' Sys.setenv(HF_API_TOKEN = "XXX") # Provide access token, if not only very limited rates are available
 #' 
 #' text = c("To be, or not to be: that is the question.", 
-#'          "Come forth into the light of things, Let Nature be your teacher.")
+#'          "An atom is a particle that consists of a nucleus of protons and neutrons surrounded by an electromagnetically-bound cloud of electrons.",
+#'          "Senate passes stopgap bill to avert government shutdown.")
 #' 
 #' hf_api(inputs = text, 
 #'        url = "https://api-inference.huggingface.co/models/MoritzLaurer/deberta-v3-large-zeroshot-v1", 
@@ -26,18 +27,25 @@ hf_api <- function(inputs, url, ...){
   require(httr);require(jsonlite)
   hf_opts = list()
   if (Sys.getenv("HF_API_TOKEN") != "") {
-    config <- add_headers(Authorization = paste("Bearer", Sys.getenv("HF_API_TOKEN")))
+    config = add_headers(Authorization = paste("Bearer", Sys.getenv("HF_API_TOKEN")))
   } else {
-    config <- NULL
+    config = NULL
   }
-  params <- c(list(inputs = inputs), list(...))
-  body = jsonlite::toJSON(c(params, hf_opts))
+  
+  params = c(list(inputs = inputs), list(...))
+  
+  if(!is.null(filename)){
+    body = read_file_raw(filename)
+  } else {
+    body = jsonlite::toJSON(c(params, hf_opts))
+  }
+  
   req = POST(url, body = body, config = config)
   response = fromJSON(content(req, "text"))
   if(is.list(response) && !is.null(response$estimated_time)){
     print(paste("Waiting", round(as.numeric(response$estimated_time)), "seconds for model to load."))
     Sys.sleep(response$estimated_time)
-    hf_api(inputs, url, ...)
+    hf_api(inputs, url, filename, ...)
   } else {
     response
   }
@@ -45,12 +53,20 @@ hf_api <- function(inputs, url, ...){
 
 
 #' @export
-text_embeddings = function(txt, url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L12-v2"){
-  hf_api(inputs = txt, url = url)
+hf_embeddings = function(txt, url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L12-v2"){
+  hf_api(inputs = txt, url = url) |> 
+    as.tibble()
 }
 
 #' @export
-text_zeroshot = function(txt, labels, url = "https://api-inference.huggingface.co/models/facebook/bart-large-mnli"){
-  hf_api(inputs = txt, url = url, parameters = list(candidate_labels = labels))
+hf_zeroshot = function(txt, labels, url = "https://api-inference.huggingface.co/models/facebook/bart-large-mnli"){
+  hf_api(inputs = txt, url = url, parameters = list(candidate_labels = labels)) |> 
+    as_tibble()
 }
 
+
+#' @export
+hf_image_classification = function(filename, url = "https://api-inference.huggingface.co/models/google/vit-base-patch16-224"){
+  hf_api(filename = filename, url = url) %>%
+    as_tibble()
+}
